2686585753dd3b488b3446cb2e0d8c0604ad5b13
justi <justi@DESKTOP-O7K8SPS.Home>
2018-08-30 00:00:47 -0400

    ok

---
+++ Areacirc.m
@@ -0,0 +1,19 @@
+clc, clear, close all
+
+% Hello World its Joel Parizi in MATLAB!
+% This is practice in preperation for my
+% machine learning course at UNG.
+
+
+
+radius = 50; %variable reference radius
+
+
+circumference = pi * 2 * radius
+
+3.14 * 8 * 8
+
+area = pi * radius *2
+
+
+% sweet! time to do some heavy duty quant trading
